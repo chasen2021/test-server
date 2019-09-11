166a5bddb4ec62adb60693bc58051e8cc20b3315
Chase Nagle <chasejnagle64@gmail.com>
2019-09-11 16:32:06 -0700

    new file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Shut up cracker</h1>
+      <h1>Shut up bitch</h1>
+      <p>Why shoud you shut up?</p>
+      <br></br>
+      <p>Its simple, becuase you are a bitch</p>
     </div>
   );
 }
