bf0a3e73b8b1c8dcf99656b3b1f0b0cb39203c69
Chase Nagle <chasejnagle64@gmail.com>
2019-09-12 01:01:39 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import Alert from 'react-bootstrap/Alert'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Shut up bitch</h1>
-      <p>Why shoud you shut up?</p>
-      <p>Its simple, becuase you are a bitch</p>
-    </div>
-  );
+
+
+
+class App extends React.Component{
+  state = {
+    text: ''
+  }
+
+  onPush = () => {
+    this.setState({
+      text:"Shut up Cracker"
+    })
+  }
+
+  render(){
+    return (
+      <body class = 'hemp'>
+      <div class = "app">
+          
+        
+        
+        <br/>
+        <h1 class = 'header'>Press the Button
+          <Badge variant = 'success'>Now</Badge>
+        </h1>
+        <br/>
+        <Button variant = 'outline-warning' size = 'lg' onClick={this.onPush}>Press</Button>
+        <br/>
+        <br/>
+        <br/>
+        <h2>{this.state.text}</h2>
+
+        
+
+
+
+      </div>
+      </body>
+    )
+  }
+
 }
+
 
 export default App;
